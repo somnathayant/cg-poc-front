943b6508fc9585e0b89ae23e5460be3c7fd1d0f4
somnath <somnath@ayantsoft.com>
2022-05-13 15:32:09 +0530

    test module added

---
+++ src/app/test/test.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { C1Component } from './c1/c1.component';
+
+
+
+@NgModule({
+  declarations: [C1Component],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestModule { }
