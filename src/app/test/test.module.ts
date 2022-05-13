7ba1a4c44c0a0e8804928a3e00e33bdfb1919cd9
somnath <somnath@ayantsoft.com>
2022-05-13 15:49:55 +0530

    c3 added

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
 
 
 
 @NgModule({
-  declarations: [C1Component, C2Component],
+  declarations: [C1Component, C2Component, C3Component],
   imports: [
     CommonModule
   ]
